94aa96ecde486c5fb78b8e2b97a0e4cadbf5b7e7
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2021-11-29 15:31:16 -0700

    Add HeScale

---
+++ hesscale/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    author="Mohamed Elsayed",
+    name="HesScale",
+    version="0.1",
+    description="Scalable Hessian Computations",
+    url="https://github.com/mhmd",
+    license="MIT",
+    packages=["hesscale"],
+    zip_safe=False,
+)
