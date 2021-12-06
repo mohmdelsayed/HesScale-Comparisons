ef5a1dced767f5ab4b23b1587d24b0301303147a
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2021-12-06 12:55:57 -0700

    update compute canada scripts

---
+++ libraries/backpack-deepobs-integration/bp_dops_integration/custom/create_scripts.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+for f in *.txt
+do
+echo "#!/bin/bash" > ${f%.*}.sh
+echo "#SBATCH --array=1-30" >> ${f%.*}.sh
+echo -e "#SBATCH --job-name="${f%.*}"\t\t\t# single job name for the array" >> ${f%.*}.sh
+echo -e "#SBATCH --time=02:00:00\t\t\t# maximum wall time per job in d-hh:mm or hh:mm:ss" >> ${f%.*}.sh
+echo -e "#SBATCH --mem=4G\t\t\t# maximum memory 100M per job" >> ${f%.*}.sh
+if [[ "${f%.*}" != *'mlp'* ]]; then
+    echo -e "#SBATCH --gres=gpu:1\t\t\t# Number of GPUs (per node)" >> ${f%.*}.sh
+fi
+echo -e "#SBATCH --account=def-ashique" >> ${f%.*}.sh
+echo -e "#SBATCH --output=%x%A%a.out\t\t\t# standard output (%A is replaced by jobID and %a with the array index)" >> ${f%.*}.sh
+echo -e "#SBATCH --error=%x%A%a.err\t\t\t# standard error\n" >> ${f%.*}.sh
+
+echo "FILE=\"\$SCRATCH/HesScale-Comparisons/experiments/grid_search_command_scripts/${f%.*}.txt\""  >> ${f%.*}.sh
+echo "SCRIPT=\$(sed -n \"\${SLURM_ARRAY_TASK_ID}p\" \$FILE)"  >> ${f%.*}.sh
+echo "module load python/3.7.9" >> ${f%.*}.sh
+echo "source \$SCRATCH/HesScale-Comparisons/env/bin/activate" >> ${f%.*}.sh
+echo "\$SCRIPT" >> ${f%.*}.sh
+done
