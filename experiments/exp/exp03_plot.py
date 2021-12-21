8dfe8f2ec8eb434c22df9626e6270dfe14a4d7ec
Mohamed Elsayed <moh@cedar5.cedar.computecanada.ca>
2021-12-20 22:53:42 -0800

    improve plotting

--- experiments/exp/exp03_plot.py
+++ experiments/exp/exp03_plot.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 import os
 import warnings
 warnings.filterwarnings("ignore")
+from matplotlib.pyplot import figure
 
-path = '../grid_search/mnist-logreg-custom/'
-my_list = os.listdir(path)[:-7]
-
+path = '../grid_search/mnist_mlp_tanh/'
+my_list = os.listdir(path)
+# my_list = ['DiagGGNMCDefaultOptimizer', 'SGDDefaultOptimizer', 'AdaHessDefaultOptimizer', 'DiagGGNDefaultOptimizer', 'HesScaleOptimizerAdamStyle', 'AdamDefaultOptimizer']
 fig, ax = plt.subplots(4, 1, sharex="col")
-[plot_optimizer_performance(path+algo, fig=fig, ax=ax, reference_path=None, show=False, metric='test_accuracies') for algo in my_list]
-plt.show()
+fig.set_size_inches(20, 12)
 
-fig, ax = plt.subplots(1, 1, sharex="col")   
-[plot_hyperparameter_sensitivity(path+algo, hyperparam='lr', xscale='log', plot_std=True, metric="test_accuracies", show=False, fig=fig, ax=ax) for algo in my_list]
-plt.show()
+[plot_optimizer_performance(path+algo, fig=fig, ax=ax, reference_path=None, show=False, metric='valid_accuracies', mode='final') for algo in my_list]
+fig.savefig('performance.png', dpi=100)
+
+# fig, ax = plt.subplots(1, 1, sharex="col")   
+# [plot_hyperparameter_sensitivity(path+algo, hyperparam='lr', xscale='log', plot_std=True, metric="valid_accuracies", show=False, fig=fig, ax=ax) for algo in my_list]
+# plt.savefig("sensitivity.png")
