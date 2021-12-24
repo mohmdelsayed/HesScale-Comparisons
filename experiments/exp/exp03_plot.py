896036c3af4348cf5d21ae55d0ac74e1f585311e
Mohamed Elsayed <moh@cedar1.cedar.computecanada.ca>
2021-12-24 10:02:25 -0800

    update plot script

--- experiments/exp/exp03_plot.py
+++ experiments/exp/exp03_plot.py
@@ -7,13 +7,15 @@ from matplotlib.pyplot import figure
 
 path = '../grid_search/mnist_mlp_tanh/'
 my_list = os.listdir(path)
-# my_list = ['DiagGGNMCDefaultOptimizer', 'SGDDefaultOptimizer', 'AdaHessDefaultOptimizer', 'DiagGGNDefaultOptimizer', 'HesScaleOptimizerAdamStyle', 'AdamDefaultOptimizer']
 fig, ax = plt.subplots(4, 1, sharex="col")
-fig.set_size_inches(20, 12)
+fig.set_size_inches(20, 16)
 
-[plot_optimizer_performance(path+algo, fig=fig, ax=ax, reference_path=None, show=False, metric='valid_accuracies', mode='final') for algo in my_list]
+[plot_optimizer_performance(path+algo, fig=fig, ax=ax, reference_path=None, show=False, metric='test_accuracies', mode='final') for algo in my_list]
 fig.savefig('performance.png', dpi=100)
 
+
 # fig, ax = plt.subplots(1, 1, sharex="col")   
+# fig.set_size_inches(20, 16)
 # [plot_hyperparameter_sensitivity(path+algo, hyperparam='lr', xscale='log', plot_std=True, metric="valid_accuracies", show=False, fig=fig, ax=ax) for algo in my_list]
-# plt.savefig("sensitivity.png")
+# fig.savefig('sensitivity.png', dpi=100)
+
