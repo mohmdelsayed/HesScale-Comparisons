d00f5d36b5a0cb080482336a3f3442b575b05d45
Mohamed Elsayed <moh@cedar1.cedar.computecanada.ca>
2021-12-24 10:03:33 -0800

    update gerenation script

--- libraries/backpack-deepobs-integration/bp_dops_integration/custom/create_scripts.bash
+++ libraries/backpack-deepobs-integration/bp_dops_integration/custom/create_scripts.bash
@@ -2,15 +2,15 @@
 for f in *.txt
 do
 echo "#!/bin/bash" > ${f%.*}.sh
-echo "#SBATCH --array=1-30" >> ${f%.*}.sh
 echo -e "#SBATCH --job-name="${f%.*}"\t\t\t# single job name for the array" >> ${f%.*}.sh
 echo -e "#SBATCH --mem=4G\t\t\t# maximum memory 100M per job" >> ${f%.*}.sh
-if [[ "${f%.*}" != *'mlp'* ]]; then
-    echo -e "#SBATCH --gres=gpu:1\t\t\t# Number of GPUs (per node)" >> ${f%.*}.sh
-    echo -e "#SBATCH --time=03:00:00\t\t\t# maximum wall time per job in d-hh:mm or hh:mm:ss" >> ${f%.*}.sh
+echo -e "#SBATCH --time=00:30:00\t\t\t# maximum wall time per job in d-hh:mm or hh:mm:ss" >> ${f%.*}.sh
+if [[ "${f%.*}" != *'SGD'* ]]; then
+echo "#SBATCH --array=1-240" >> ${f%.*}.sh
 else
-    echo -e "#SBATCH --time=02:00:00\t\t\t# maximum wall time per job in d-hh:mm or hh:mm:ss" >> ${f%.*}.sh
+echo "#SBATCH --array=1-40" >> ${f%.*}.sh
 fi
+echo -e "#SBATCH --gres=gpu:1\t\t\t# Number of GPUs (per node)" >> ${f%.*}.sh
 echo -e "#SBATCH --account=def-ashique" >> ${f%.*}.sh
 echo -e "#SBATCH --output=%x%A%a.out\t\t\t# standard output (%A is replaced by jobID and %a with the array index)" >> ${f%.*}.sh
 echo -e "#SBATCH --error=%x%A%a.err\t\t\t# standard error\n" >> ${f%.*}.sh
